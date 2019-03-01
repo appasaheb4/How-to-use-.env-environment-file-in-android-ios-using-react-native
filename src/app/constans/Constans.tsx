043e154f80992b76e3b609ffe59d6be9a6f97bd5
appa3151 <onlyappasaheb4@gmail.com>
2019-03-01 09:48:15 +0530

    project create

---
+++ src/app/constans/Constans.tsx
@@ -0,0 +1,13 @@
+import Config from "react-native-config";
+
+//MESSAGES
+var msg = {
+  userEmail: Config.APP_EMAIL
+};
+
+//Colors
+var colors = {
+  appColor: Config.COLORS_APP_COLOR
+};
+
+export { msg, colors };
